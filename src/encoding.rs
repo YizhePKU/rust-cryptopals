033281ab1d29bb179f8f9a773ef7806ac06f1147
YizhePKU <yizhe@pku.edu.cn>
2023-06-01 03:06:29 +0800

    Implement b64 and hex convertions

---
+++ src/encoding.rs
@@ -0,0 +1,51 @@
+use base64::{engine::general_purpose::STANDARD, Engine};
+
+pub fn hex_encode(bytes: &[u8]) -> String {
+    hex::encode(bytes)
+}
+
+pub fn hex_decode(text: &str) -> Vec<u8> {
+    hex::decode(text).unwrap()
+}
+
+pub fn b64_encode(bytes: &[u8]) -> String {
+    STANDARD.encode(bytes)
+}
+
+pub fn b64_decode(text: &str) -> Vec<u8> {
+    STANDARD.decode(text).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    static ANYBYTE: prop::num::u8::Any = prop::num::u8::ANY;
+
+    #[test]
+    fn cryptopals_s1c1() {
+        let bytes = hex_decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+        let text = b64_encode(&bytes);
+        assert_eq!(
+            text,
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        );
+    }
+
+    proptest! {
+        #[test]
+        fn hex_roundtrip(bytes in [ANYBYTE; 40]) {
+            let hex = hex_encode(&bytes);
+            let bytes2 = hex_decode(&hex);
+            assert_eq!(&bytes, bytes2.as_slice());
+        }
+
+        #[test]
+        fn b64_roundtrip(bytes in [ANYBYTE; 40]) {
+            let b64 = b64_encode(&bytes);
+            let bytes2 = b64_decode(&b64);
+            assert_eq!(&bytes, bytes2.as_slice());
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod aes_128;
+pub mod encoding;
