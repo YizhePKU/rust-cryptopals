b8b8980b38886a2d6d484cfc00842626c6277de6
YizhePKU <yizhe@pku.edu.cn>
2023-06-01 08:35:08 +0800

    Implement pkcs7

--- src/utils.rs
+++ src/utils.rs
@@ -8,11 +8,36 @@ pub fn xor(lhs: &[u8], rhs: &[u8]) -> Vec<u8> {
     result
 }
 
+pub fn pkcs7(bytes: &[u8]) -> Vec<u8> {
+    const K: usize = 16;
+
+    let mut result = bytes.to_owned();
+    let cnt = K - (bytes.len() % K);
+    for _ in 0..cnt {
+        result.push(cnt as u8);
+    }
+
+    result
+}
+
+pub fn inv_pkcs7(bytes: &[u8]) -> Result<Vec<u8>, &'static str> {
+    let cnt = bytes[bytes.len() - 1] as usize;
+    if cnt == 0 || cnt > 16 {
+        return Err("Bad padding");
+    }
+    for i in 0..cnt {
+        if bytes[bytes.len() - 1 - i] != cnt as u8 {
+            return Err("Bad padding");
+        }
+    }
+    Ok(bytes[..bytes.len() - cnt].to_owned())
+}
+
 #[cfg(test)]
 mod test {
-    use crate::encoding::hex_decode;
-
     use super::*;
+    use crate::encoding::hex_decode;
+    use proptest::prelude::*;
 
     #[test]
     fn cryptopals_s1c2() {
@@ -21,5 +46,14 @@ mod test {
         let result = hex_decode("746865206b696420646f6e277420706c6179");
 
         assert_eq!(xor(&lhs, &rhs), result);
+    }
+
+    proptest! {
+        #[test]
+        fn pkcs7_roundtrip(len in prop::num::u16::ANY) {
+            let data = vec![0; len as usize];
+            let data2 = inv_pkcs7(&pkcs7(&data)).unwrap();
+            assert_eq!(data, data2);
+        }
     }
 }
